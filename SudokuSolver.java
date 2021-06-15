04de849611652d70b274f26cb63122ba852b2a35
nujoom17 <83825670+nujoom17@users.noreply.github.com>
2021-06-15 12:59:38 +0400

    Update SudokuSolver.java
    This is the code that will take user inputs (in the form of a 9x9 2-d array) and see if they can be solved in the form of a sudoku. The program will return a negative print statement in case the numbers are invalid or unsolvable

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -1,29 +1,115 @@
-
 import java.util.Scanner;
 
-public class Main {
+public class SudokuSolver {
 
 
-    public static void main(String[] args) {
-        Scanner choice=new Scanner(System.in);
+        private static final int GRID_SIZE = 9;
 
-        System.out.println("""
-                Hello! Choose from the options below to play some games\s
-                1. Quiz
-                2. Sudoku Solver""");
-        int n=choice.nextInt();
-        switch(n){
-            case 1:
-                Quiz.TakeTest();
-                break;
-            case 2:
-                SudokuSolver.main();
-                break;
+        public static void main() {
+            Scanner in=new Scanner(System.in);
 
+            int[][] board = new int[9][9];
+            System.out.println("Enter the 9x9 Elements (Row-wise)");
+            for(int i=0;i<board.length;i++){
+                int x=i+1;
+                System.out.println("Enter 9 elements for row number "+x+" separated after hitting enter");
+                for(int j=0;j<board[0].length;j++){
+                    board[i][j]=in.nextInt();
+                }
+            }
+
+            printBoard(board);
+
+            if (solveBoard(board)) {
+                System.out.println("Solved successfully!");
+            }
+            else {
+                System.out.println("Unsolvable board!");
+            }
+
+            printBoard(board);
 
         }
 
 
+        private static void printBoard(int[][] board) {
+            for (int row = 0; row < GRID_SIZE; row++) {
+                if (row % 3 == 0 && row != 0) {
+                    System.out.println("-----------");
+                }
+                for (int column = 0; column < GRID_SIZE; column++) {
+                    if (column % 3 == 0 && column != 0) {
+                        System.out.print("|");
+                    }
+                    System.out.print(board[row][column]);
+                }
+                System.out.println();
+            }
+        }
+
+
+        private static boolean isNumberInRow(int[][] board, int number, int row) {
+            for (int i = 0; i < GRID_SIZE; i++) {
+                if (board[row][i] == number) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static boolean isNumberInColumn(int[][] board, int number, int column) {
+            for (int i = 0; i < GRID_SIZE; i++) {
+                if (board[i][column] == number) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static boolean isNumberInBox(int[][] board, int number, int row, int column) {
+            int localBoxRow = row - row % 3;
+            int localBoxColumn = column - column % 3;
+
+            for (int i = localBoxRow; i < localBoxRow + 3; i++) {
+                for (int j = localBoxColumn; j < localBoxColumn + 3; j++) {
+                    if (board[i][j] == number) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
+            return !isNumberInRow(board, number, row) &&
+                    !isNumberInColumn(board, number, column) &&
+                    !isNumberInBox(board, number, row, column);
+        }
+
+        private static boolean solveBoard(int[][] board) {
+            for (int row = 0; row < GRID_SIZE; row++) {
+                for (int column = 0; column < GRID_SIZE; column++) {
+                    if (board[row][column] == 0) {
+                        for (int numberToTry = 1; numberToTry <= GRID_SIZE; numberToTry++) {
+                            if (isValidPlacement(board, numberToTry, row, column)) {
+                                board[row][column] = numberToTry;
+
+                                if (solveBoard(board)) {
+                                    return true;
+                                }
+                                else {
+                                    board[row][column] = 0;
+                                }
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+
+
     }
-}
 
